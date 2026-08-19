6e7547f117dbe741ce5f0920cc242e250d199705
Saini <komasaini.ext@deloitte.com>
2026-08-19 14:46:47 +0530

    feat: configure progressive web app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App.tsx";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error("Root element not found.");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", (): void => {
+    void navigator.serviceWorker.register("/sw.js");
+  });
+}
